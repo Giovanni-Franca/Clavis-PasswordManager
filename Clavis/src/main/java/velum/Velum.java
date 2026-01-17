c1b63b41a134a169e455b85e482a29efd154d5e2
Giovanni <Giovanni@DESKTOP-604HS2D>
2026-01-17 15:31:19 -0300

    Alterações visuais

---
+++ Clavis/src/main/java/velum/Velum.java
@@ -0,0 +1,197 @@
+package velum;
+
+import java.awt.CardLayout;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.Toolkit;
+
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+public class Velum extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Velum frame = new Velum();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	
+	CardLayout cardLayout = new CardLayout();
+    JPanel pPrincipal = new JPanel(cardLayout);
+    
+	public Velum() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage("E:\\eclipse-workspace\\Clavis\\src\\main\\resources\\chave.png"));
+		setTitle("Gerenciador de senhas");
+		setBounds(0, 0, 920, 480);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.X_AXIS));
+		
+	// Menu lateral para navegação
+		JPanel menuLateral = new JPanel(new GridLayout(5, 2, 0, 5));
+		menuLateral.setBounds(0, 0, 120, 442);
+
+		JButton btnHome = new JButton("Home");
+        JButton btnNovaSenha = new JButton("Nova Senha");
+
+        menuLateral.add(btnHome);
+        menuLateral.add(btnNovaSenha);
+
+     // adiciona ação aos botões
+        btnHome.addActionListener(e-> cardLayout.show(pPrincipal, "home"));
+        btnNovaSenha.addActionListener(e -> cardLayout.show(pPrincipal, "nova senha"));
+        
+	 // criação das "telas" de acordo com os botões
+        JPanel pHome = new JPanel();
+        JPanel pNovaSenha = new JPanel();
+        
+        pPrincipal.setBounds(118, 0, 676, 442);
+
+        pPrincipal.add(pHome, "home");
+        pPrincipal.add(pNovaSenha, "nova senha");
+        
+        // Painel inicial que mostra as senhas
+        pHome.setLayout(new GridLayout(10, 3, 10, 10));
+        pHome.add(new JLabel("Teste"));
+        
+                
+        // Painel para adição das senhas
+        
+        GridBagLayout gbl_pNovaSenha = new GridBagLayout();
+        pNovaSenha.setLayout(gbl_pNovaSenha);
+        
+        
+        JLabel lbl_Nome = new JLabel("Nome");
+        GridBagConstraints gbc_lbl_Nome = new GridBagConstraints();
+        gbc_lbl_Nome.insets = new Insets(0, 0, 10, 5);
+        gbc_lbl_Nome.gridx = 0;
+        gbc_lbl_Nome.gridy = 1;
+        pNovaSenha.add(lbl_Nome, gbc_lbl_Nome);
+        
+        JLabel lbl_Tamanho = new JLabel("Tamanho");
+        GridBagConstraints gbc_lbl_Tamanho = new GridBagConstraints();
+        gbc_lbl_Tamanho.insets = new Insets(0, 0, 10, 0);
+        gbc_lbl_Tamanho.gridx = 3;
+        gbc_lbl_Tamanho.gridy = 1;
+        pNovaSenha.add(lbl_Tamanho, gbc_lbl_Tamanho);
+        
+        JTextField txt_nome = new JTextField();
+        GridBagConstraints gbc_txt_nome = new GridBagConstraints();
+        gbc_txt_nome.insets = new Insets(0, 0, 20, 5);
+        gbc_txt_nome.fill = GridBagConstraints.HORIZONTAL;
+        gbc_txt_nome.gridx = 0;
+        gbc_txt_nome.gridy = 2;
+        pNovaSenha.add(txt_nome, gbc_txt_nome);
+        txt_nome.setPreferredSize(new Dimension(180,24));;
+        
+        JSpinner spinner = new JSpinner();
+        GridBagConstraints gbc_spinner = new GridBagConstraints();
+        gbc_spinner.insets = new Insets(0, 0, 20, 0);
+        gbc_spinner.gridx = 3;
+        gbc_spinner.gridy = 2;
+        pNovaSenha.add(spinner, gbc_spinner);
+        spinner.setPreferredSize(new Dimension(65,24));
+        
+        JLabel lbl_Senha = new JLabel("Senha");
+        GridBagConstraints gbc_lbl_Senha = new GridBagConstraints();
+        gbc_lbl_Senha.insets = new Insets(0, 0, 10, 5);
+        gbc_lbl_Senha.gridx = 1;
+        gbc_lbl_Senha.gridy = 3;
+        pNovaSenha.add(lbl_Senha, gbc_lbl_Senha);
+        
+        JTextField txt_senha = new JTextField();
+        GridBagConstraints gbc_txt_senha = new GridBagConstraints();
+        gbc_txt_senha.insets = new Insets(0, 0, 50, 5);
+        gbc_txt_senha.fill = GridBagConstraints.HORIZONTAL;
+        gbc_txt_senha.gridx = 1;
+        gbc_txt_senha.gridy = 4;
+        pNovaSenha.add(txt_senha, gbc_txt_senha);
+        txt_senha.setPreferredSize(new Dimension(210,24));
+        
+        JButton btn_SenhaAleatoria = new JButton("");
+        btn_SenhaAleatoria.setHorizontalAlignment(SwingConstants.LEFT);
+        btn_SenhaAleatoria.setToolTipText("Gerar senha");
+        btn_SenhaAleatoria.setIcon(new ImageIcon(getClass().getResource("/Iconsmind-Outline-Reload-2-2.16.png")));
+        GridBagConstraints gbc_btn_SenhaAleatoria = new GridBagConstraints();
+        gbc_btn_SenhaAleatoria.anchor = GridBagConstraints.NORTHWEST;
+        gbc_btn_SenhaAleatoria.insets = new Insets(0, 0, 5, 5);
+        gbc_btn_SenhaAleatoria.gridx = 2;
+        gbc_btn_SenhaAleatoria.gridy = 4;
+        pNovaSenha.add(btn_SenhaAleatoria, gbc_btn_SenhaAleatoria);
+        
+        JLabel lbl_aviso = new JLabel("");
+        GridBagConstraints gbc_lbl_aviso = new GridBagConstraints();
+        gbc_lbl_aviso.insets = new Insets(0, 0, 5, 5);
+        gbc_lbl_aviso.gridx = 1;
+        gbc_lbl_aviso.gridy = 5;
+        pNovaSenha.add(lbl_aviso, gbc_lbl_aviso);
+        
+        JButton btnSalvar = new JButton("Salvar Senha");
+        GridBagConstraints gbc_btnSalvar = new GridBagConstraints();
+        gbc_btnSalvar.insets = new Insets(0, 0, 0, 5);
+        gbc_btnSalvar.gridx = 1;
+        gbc_btnSalvar.gridy = 6;
+        pNovaSenha.add(btnSalvar, gbc_btnSalvar);
+       
+        // verifica se a senha possui caracteres recomendados e exibe uma mensagem
+        txt_senha.getDocument().addDocumentListener(new DocumentListener(){			
+			@Override
+			public void insertUpdate(DocumentEvent e) {		
+				processar();
+			}
+			
+			@Override
+			public void removeUpdate(DocumentEvent e) {		
+				processar();
+			}
+			
+			@Override
+			public void changedUpdate(DocumentEvent e) {
+				processar();
+			}	
+			private void processar() {
+				String senha = txt_senha.getText();
+				lbl_aviso.setText(Munus.validate(senha));
+			}
+		}
+		);
+        
+        
+    // Adiciona as telas ao aplicativo
+        getContentPane().add(menuLateral);
+        getContentPane().add(pPrincipal);
+        
+        setVisible(true);
+    }
+
+}
