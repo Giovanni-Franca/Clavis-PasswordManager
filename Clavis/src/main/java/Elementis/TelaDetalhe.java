6a7732f14528bc4765ce1dfff431f32bf41b3ec8
Giovanni <Giovanni@DESKTOP-604HS2D>
2026-02-03 14:58:16 -0300

    reupload

---
+++ Clavis/src/main/java/Elementis/TelaDetalhe.java
@@ -0,0 +1,52 @@
+package Elementis;
+
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import Entitas.Senha;
+import velum.Munus;
+
+public class TelaDetalhe extends JDialog{
+	public TelaDetalhe(Senha s) {
+		setTitle("Detalhes");
+		setSize(300,200);
+		setLocationRelativeTo(null);
+		setLayout(new GridLayout(0,1));
+		add(new JLabel("nome: " +s.getNome()));
+		add(new JLabel("Senha: "+s.getSenha()));
+		
+		JButton btnExcluir = new JButton("Excluir");
+		btnExcluir.addActionListener(new ActionListener() {
+			public void actionPerformed (ActionEvent e) {
+				int result = JOptionPane.showConfirmDialog(null,"Quer mesmo exlcuir esta senha?", 
+						"Excluir", 
+						JOptionPane.YES_NO_OPTION,
+						JOptionPane.WARNING_MESSAGE);
+				if (result == JOptionPane.YES_OPTION) {
+					Munus.excluirSenha(s.getID());
+					
+				}
+			
+			}
+		});
+		
+		JButton btnCopiar = new JButton("Copiar");
+		btnCopiar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				StringSelection senha = new StringSelection(s.getSenha());
+				Toolkit.getDefaultToolkit().getSystemClipboard().setContents(senha, null);;
+			}
+		});
+		add(btnExcluir);
+		add(btnCopiar);
+		setVisible(true);
+	}
+}
